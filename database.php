d91497a96c9e91dc2ab397f79a1618831d5528c7
jerome <ambroise.jerome.formation@gmail.com>
2019-03-07 17:02:38 +0100

    Affichage des produits dans une card bootstrap

--- database.php
+++ database.php
@@ -5,5 +5,33 @@ use Classes\Database\Connection;
 
 $connection = new Connection();
 
-$tableau = $connection->query('SELECT * FROM product');
-var_dump($tableau);
+$products = $connection->query(
+    'SELECT * FROM product'
+);
+
+// HTML
+include 'header.php'; ?>
+
+<main class="container">
+    <h1>Liste des produits</h1>
+
+    <?php foreach($products as $product) : ?>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"><?= $product['name']; ?></h5>
+                <p class="card-text"><?= $product['description']; ?></p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+</main>
+
+<?php include 'footer.html'; ?>
+
+
+
+
+
+
+
